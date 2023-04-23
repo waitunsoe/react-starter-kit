05681add4501963b259d3c3726cc95e4bbef7091
waihtunsoe <waitunsoe.dev@gmail.com>
2023-04-23 13:39:12 +0630

    Revert "Merge pull request #1 from waitunsoe/themealdb"
    This reverts commit 49c8eb413c498506e4b8825c28800467ac9f1824, reversing
    changes made to cc8729f47a4ba88d2f41a535ddad603364ac1e58.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import React from "react";
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Meal from "./components/Meal";
-import DetailMeal from "./components/DetailMeal";
-import Test from "./components/Test";
-import Navbar from "./components/Navbar";
-import Search from "./components/Search";
+import React from 'react'
+import "./App.css"
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Meal />} />
-        <Route path="/detail/:id" element={<DetailMeal />} />
-        <Route path="/search/:name" element={<Search />} />
-        <Route path="/test" element={<Test />} />
-      </Routes>
-    </>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
